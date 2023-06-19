ebf058698d1e0477981d04d430900b4c243704fa
CuriousR82 <rosajeon82@gmail.com>
2023-06-19 13:10:16 -0400

    Initial Commit

---
+++ ExploreMapkit/ExploreMapkitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExploreMapkitApp.swift
+//  ExploreMapkit
+//
+//  Created by Rosa Jeon on 2023-06-19.
+//
+
+import SwiftUI
+
+@main
+struct ExploreMapkitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
